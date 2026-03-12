49abf38fbec5bb6066824ff1be455b25bdf7fbe1
Eason0729 <30045503+Eason0729@users.noreply.github.com>
2026-03-12 15:01:18 +0800

    fix: build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,10 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("linux") {
-        let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-        println!(
-            "cargo:rustc-link-arg=-Wl,--dynamic-list={}/pam_dynamic_list.txt",
-            dir
-        );
-        println!("cargo:rerun-if-changed=pam_dynamic_list.txt");
+    let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!(
+        "cargo:rustc-link-arg=-Wl,--dynamic-list={}/pam_dynamic_list.txt",
+        dir
+    );
+    println!("cargo:rerun-if-changed=pam_dynamic_list.txt");
 
-        println!("cargo:rustc-link-arg=/lib64/libpam.so.0");
-    }
+    println!("cargo:rustc-link-arg=/lib64/libpam.so.0");
 }
